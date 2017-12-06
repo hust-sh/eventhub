02c97dd5dd251cfc2ae668000e74836bdbf824d2
shenweimin <shenweimin@bytedance.com>
2017-12-06 10:51:33 +0800

    feat: github oauth
    Change-Id: I4f7f02146fc53c2df7cefb3407dfda51cb75a27b

--- eventhub/config.go
+++ eventhub/config.go
@@ -1,3 +1,22 @@
 package main
 
+import (
+	"golang.org/x/oauth2"
+	"os"
+	"golang.org/x/oauth2/github"
+)
+
 var RedisUrl = "redis://redis:6379"
+
+var OauthCallbacks = map[string]string{}
+
+var (
+    githubOauthConfig = &oauth2.Config {
+                        RedirectURL:  "http://localhost:3003/callback/github",
+                        ClientID:     os.Getenv("GITHUBKET"), 
+                        ClientSecret: os.Getenv("GITHUB_SECRET"),
+                        Scopes:       []string{"user", "gist"},
+                        Endpoint:     github.Endpoint}
+    githubStateString = "mock"
+)
+
